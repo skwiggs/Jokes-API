6c9b5f28824db7db632e07ccb493c2b6764180a9
Callum Barclay <hi@callumbarclay.scot>
2015-06-26 13:56:21 +0100

    Create jokes.php

---
+++ jokes.php
@@ -0,0 +1,17 @@
+<?php
+$jokes = Array(
+    "Did you hear about the guy who invented Lifesavers? They say he made a mint.",
+    "Did you hear the one about the crow and the telephone pole?He wanted to make a long distance caw.",
+    "What do you get when you mix human dna with a goats'? Banned from the petting zoo.",
+    "When Chuck Norris does push-ups, he doesn't push himself up, he pushes the world down.",
+    "Chuck norris does not own a passport. The country he visits must meet his requirements.",
+    "I love playing American Football with my son, but recently he has become too big to throw.",
+    "How do you catch a unique rabbit? Unique up on it.",
+    "Diplomacy is the ability to tell someone to \"get lost\" in such a way that they look forward to the trip.",
+    "The definition of windows 95: 32 bit extensions and a graphical shell for a 16 bit patch to an 8 bit operating system originally coded for a 4 bit microprocessor, written by a 2 bit company, that can't stand 1 bit of competition.",
+    "Why are ghosts bad liars? Because you can see right through them.",
+    "What's black and white and makes a lot of noise? A zebra with a drumkit.",
+    "What's white, black, and red all over? A penguin in a liquidiser"
+);
+
+echo $jokes[ array_rand( $jokes ) ];
